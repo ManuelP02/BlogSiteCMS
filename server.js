96e4e7586e0fb2adb765472fcd32a367459cb14b
ManuelP02 <manuelpena0207@gmail.com>
2024-08-04 16:28:38 -0400

    User and post routes working

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+// Server things
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const session = require('express-session');
+//Sequelize var
+const sequelize = require('./config/connection');
 
+//Calling the models
+const model = require('./models');
 
-app.listen(PORT, ()=>{
-    console.log(`Server Running on port ${PORT}`);
-})
+//Calling the routes
+const routes = require('./controllers');
+
+//MiddleWare
+app.use(session({
+    secret: 'your_secret_key_here',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true } 
+  }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.use(routes);
+
+sequelize.sync({force: true}).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server Running on port ${PORT}`);
+    })
+});
+
+    
