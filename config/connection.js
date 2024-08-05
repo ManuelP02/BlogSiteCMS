f5c58519d1cd0f6705f37e5541879babca01d96e
ManuelP02 <manuelpena0207@gmail.com>
2024-08-05 13:16:29 -0400

    Configured connection file to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,28 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
+let sequelize
+
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
         host: 'localhost',
         dialect: 'postgres',
-    }
-);
-module.exports = sequelize;
+        dialectOptions:{
+          charset: 'utf8mb4'
+        }, 
+        define:{
+          charset: 'utf8mb4',
+          collate: 'utf8mb4_unicode_ci',
+        },
+      }
+    );
+  }
+  
+  module.exports = sequelize;
